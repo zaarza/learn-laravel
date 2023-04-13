3b8c3334bb454e8dd841dbf9d212a57f7f49a453
zaarza <zaarza03@gmail.com>
2023-04-13 19:07:21 +0700

    Folder structure, Routes & View

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Halaman Home</h1>
+
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/blog">Blog</a></li>
+    </ul>
+    
+    <button onclick="sayHello()">Say Hello</button>
+    <script src="assets/scripts/main.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
+Route::get('/about', function () {
+    $data = [
+        'name' => 'Arza',
+        'email' => 'email@email.com',
+        'image' => 'profile.jpg'
+    ];
+
+    return view('about', $data);
 });
